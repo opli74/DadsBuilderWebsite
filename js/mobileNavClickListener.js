57ab0be314a1db6ecaea9832096873b75944c17e
opli <gaggersgag@gmail.com>
2022-12-09 20:46:32 +0000

    adafa

--- js/mobileNavClickListener.js
+++ js/mobileNavClickListener.js
@@ -9,14 +9,24 @@ function clickEvent() {
     parent.classList.toggle("parent");
 }
 
+function mobileMediaFunction(x)
+{
+    if (x.matches)
+    {
+        const outsideClickListener = (event) => {
+            const $target = $(event.target);
+            if (!$target.closest(parentList).length) {
+                navAbout.classList.toggle("margin-top");
+                showChildren.classList.toggle("children");
+                parent.classList.toggle("parent")
+            }
+        }
 
-const outsideClickListener = (event) => {
-    const $target = $(event.target);
-    if (!$target.closest(parentList).length) {
-        navAbout.classList.toggle("margin-top");
-        showChildren.classList.toggle("children");
-        parent.classList.toggle("parent")
+        document.addEventListener('click', outsideClickListener);
     }
 }
 
-document.addEventListener('click', outsideClickListener);
+var x = window.matchMedia("(max-width: 1000px)");
+mobileMediaFunction(x);
+x.addListener(mobileMediaFunction);
+
