8a045b6e19a93d52ce72cdd3e078214f5948b2c2
opli <gaggersgag@gmail.com>
2022-12-17 02:00:16 +0000

    zz

--- js/mobileNavClickListener.js
+++ js/mobileNavClickListener.js
@@ -1,13 +1,4 @@
-const navAbout = document.querySelector("#navAbout");
-const showChildren = document.querySelector("#childrenWrapper");
-const parent = document.querySelector(".nav-subcontent-parent")
-const parentList = document.querySelectorAll(".nav-subcontent-wrapper")[0];
 
-function clickEvent() {
-    jQuery(navAbout).toggleClass("margin-top");
-    jQuery(showChildren).toggleClass("children");
-    jQuery(parent).toggleClass("parent");
-}
 
 // const outsideClickListener = (event) => {
 //     const $target = $(event.target);
