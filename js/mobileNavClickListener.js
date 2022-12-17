363681512eb49205775a4a1f52d5c1fe3666b31a
opli <gaggersgag@gmail.com>
2022-12-17 01:31:21 +0000

    updates

--- js/mobileNavClickListener.js
+++ js/mobileNavClickListener.js
@@ -1,32 +1,22 @@
-const navAbout = document.querySelector("#navAbout");
-const showChildren = document.querySelector("#childrenWrapper");
-const parent = document.querySelector(".nav-subcontent-parent")
-const parentList = document.querySelector(".nav-subcontent-wrapper");
+const navAbout = document.querySelectorAll("#navAbout");
+const showChildren = document.querySelectorAll("#childrenWrapper");
+const parent = document.querySelectorAll(".nav-subcontent-parent")
+const parentList = document.querySelectorAll(".nav-subcontent-wrapper");
 
 function clickEvent() {
-    navAbout.classList.toggle("margin-top");
-    showChildren.classList.toggle("children");
-    parent.classList.toggle("parent");
+    jQuery(navAbout).toggleClass("margin-top");
+    jQuery(showChildren).toggleClass("children");
+    jQuery(parent).toggleClass("parent");
 }
 
-function mobileMediaFunction(x)
-{
-    if (x.matches)
-    {
-        const outsideClickListener = (event) => {
-            const $target = $(event.target);
-            if (!$target.closest(parentList).length) {
-                navAbout.classList.toggle("margin-top");
-                showChildren.classList.toggle("children");
-                parent.classList.toggle("parent")
-            }
-        }
-
-        document.addEventListener('click', outsideClickListener);
+const outsideClickListener = (event) => {
+    const $target = $(event.target);
+    if (!$target.closest(parentList).length) {
+        jQuery(navAbout).toggleClass("margin-top");
+        jQuery(showChildren).toggleClass("children");
+        jQuery(parent).toggleClass("parent")
     }
 }
 
-var x = window.matchMedia("(max-width: 1000px)");
-mobileMediaFunction(x);
-x.addListener(mobileMediaFunction);
+document.addEventListener('click', outsideClickListener);
 
