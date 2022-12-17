434fad939e6e267a66c842ec2bc1c4fe5b592f82
opli <gaggersgag@gmail.com>
2022-12-17 01:57:28 +0000

    aa

--- js/mobileNavClickListener.js
+++ js/mobileNavClickListener.js
@@ -9,14 +9,14 @@ function clickEvent() {
     jQuery(parent).toggleClass("parent");
 }
 
-const outsideClickListener = (event) => {
-    const $target = $(event.target);
-    if (!$target.closest(parentList).length) {
-        jQuery(navAbout).toggleClass("margin-top");
-        jQuery(showChildren).toggleClass("children");
-        jQuery(parent).toggleClass("parent")
-    }
-}
+// const outsideClickListener = (event) => {
+//     const $target = $(event.target);
+//     if (!$target.closest(parentList).length) {
+//         jQuery(navAbout).toggleClass("margin-top");
+//         jQuery(showChildren).toggleClass("children");
+//         jQuery(parent).toggleClass("parent")
+//     }
+// }
 
-document.addEventListener('click', outsideClickListener);
+// document.addEventListener('click', outsideClickListener);
 
